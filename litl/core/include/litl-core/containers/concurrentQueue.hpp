f790d16e5947e64d099da8ae6da0ff821d04a4fd
Steven Sell <ssell@vertexfragment.com>
2026-02-02 00:00:22 -0500

    Start on concurrent queues

---
+++ litl/core/include/litl-core/containers/concurrentQueue.hpp
@@ -0,0 +1,18 @@
+#ifndef LITL_CORE_CONTAINERS_CONCURRENT_QUEUE_H__
+#define LITL_CORE_CONTAINERS_CONCURRENT_QUEUE_H__
+
+namespace LITL::Core::Containers
+{
+    /// <summary>
+    /// Implementation of a Multiple-Producer, Multiple-Consumer (MPMC) thread-safe queue.
+    /// </summary>
+    class ConcurrentQueue
+    {
+    public:
+
+    protected:
+
+    private:
+    };
+}
+#endif
