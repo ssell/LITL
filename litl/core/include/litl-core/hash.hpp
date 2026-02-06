b61045a20073452cb82f392a517966e18bfae4f7
Steven Sell <ssell@vertexfragment.com>
2026-02-06 17:35:19 -0500

    Hash combine, hash pod, hash array

--- litl/core/include/litl-core/hash.hpp
+++ litl/core/include/litl-core/hash.hpp
@@ -2,6 +2,8 @@
 #define LITL_CORE_HASH_H__
 
 #include <cstdint>
+#include <span>
+#include <type_traits>
 
 namespace LITL::Core
 {
@@ -10,6 +12,37 @@ namespace LITL::Core
 
     uint64_t hash64(void const* data, size_t length);
     uint64_t hash64(void const* data, size_t length, uint64_t seed);
+
+    void hashCombine32(uint32_t& lhs, uint32_t rhs);
+    void hashCombine64(uint64_t& lhs, uint64_t rhs);
+    
+    template<typename T>
+    uint64_t hashPOD(T const& pod)
+    {
+        static_assert(std::is_trivially_copyable_v<T>);
+        return hash64(&pod, sizeof(T));
+    }
+
+    template<typename T>
+    uint64_t hashPOD(T const& pod, uint64_t seed)
+    {
+        static_assert(std::is_trivially_copyable_v<T>);
+        return hash64(&pod, sizeof(T), seed);
+    }
+
+    template<typename T>
+    uint64_t hashArray(std::span<T const> data)
+    {
+        static_assert(std::is_trivially_copyable_v<T>);
+        return hash64(data.data(), data.size_bytes());
+    }
+
+    template<typename T>
+    uint64_t hashArray(std::span<T const> data, uint64_t seed)
+    {
+        static_assert(std::is_trivially_copyable_v<T>);
+        return hash64(data.data(), data.size_bytes(), seed);
+    }
 }
 
 #endif
--- litl/core/src/litl-core/hash.cpp
+++ litl/core/src/litl-core/hash.cpp
@@ -1,25 +1,47 @@
+﻿#define XXH_STATIC_LINKING_ONLY
+#define XXH_IMPLEMENTATION
+
+#include <xxhash.h>
 #include "litl-core/hash.hpp"
-#include "xxhash.h"
 
 namespace LITL::Core
 {
+    // (2^32 / φ) (golden ratio) and (2^64 / φ) respectively where φ = golden ratio (~1.6180339887)
+    constexpr uint32_t goldenRatioHash32 = 0x9e3779b9UL;
+    constexpr uint64_t goldenRatioHash64 = 0x9e3779b97f4a7c15ULL;
+
+    constexpr XXH32_hash_t default32Seed = 321337;
+    constexpr XXH64_hash_t default64Seed = 641337;
+
     uint32_t hash32(void const* data, size_t length)
     {
-        return XXH32(data, length, 321337);
+        return XXH32(data, length, default32Seed);
     }
 
     uint32_t hash32(void const* data, size_t length, uint32_t seed)
     {
-        return XXH32(data, length, seed);
+        return XXH32(data, length, static_cast<XXH32_hash_t>(seed));
     }
 
     uint64_t hash64(void const* data, size_t length)
     {
-        return XXH64(data, length, 641337);
+        return XXH64(data, length, default64Seed);
     }
 
     uint64_t hash64(void const* data, size_t length, uint64_t seed)
     {
-        return XXH64(data, length, seed);
+        return XXH64(data, length, static_cast<XXH64_hash_t>(seed));
+    }
+
+    void hashCombine32(uint32_t& lhs, uint32_t rhs)
+    {
+        // Golden ratio is used as it is irrational and distributes bits evenly.
+        // The shifts are to mix high bits into low bits to prevent early field domination.
+        lhs ^= rhs + goldenRatioHash32 + (lhs << 6) + (lhs >> 2);
+    }
+
+    void hashCombine64(uint64_t& lhs, uint64_t rhs)
+    {
+        lhs ^= rhs + goldenRatioHash64 + (lhs << 6) + (lhs >> 2);
     }
 }
