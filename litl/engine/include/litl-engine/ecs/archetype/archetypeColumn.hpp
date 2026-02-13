a66c1d87539bba00a65d805bf0ad33b340148523
Steven Sell <ssell@vertexfragment.com>
2026-02-13 14:30:37 -0500

    ArchetypeColumn

--- litl/engine/include/litl-engine/ecs/archetype/archetypeColumn.hpp
+++ litl/engine/include/litl-engine/ecs/archetype/archetypeColumn.hpp
@@ -1,9 +1,76 @@
 #ifndef LITL_ENGINE_ECS_ARCHETYPE_COLUMN_H__
 #define LITL_ENGINE_ECS_ARCHETYPE_COLUMN_H__
 
+#include <cstdint>
+
+#include "litl-core/impl.hpp"
+#include "litl-engine/ecs/component.hpp"
+
 namespace LITL::Engine::ECS
 {
-    // ... todo ...
+    /// <summary>
+    /// Storage for a singular component type in an Archetype.
+    /// The component is described (and managed) by its ComponentDescriptor.
+    /// </summary>
+    class ArchetypeColumn
+    {
+    public:
+
+        /// <summary>
+        /// If capacity is zero then it is lazily allocated at time of first call to add.
+        /// </summary>
+        /// <param name="pDescriptor"></param>
+        /// <param name="capacity"></param>
+        ArchetypeColumn(ComponentDescriptor const* pDescriptor, size_t capacity);
+        ArchetypeColumn(ArchetypeColumn const&) = delete;
+        ArchetypeColumn& operator=(ArchetypeColumn const&) = delete;
+
+        /// <summary>
+        /// Increases the size of the internal buffer to specified size.
+        /// </summary>
+        /// <param name="newCapacity"></param>
+        void reserve(size_t newCapacity);
+
+        /// <summary>
+        /// Adds another component instance to the buffer.
+        /// If the buffer is at capacity, the capacity is increased first.
+        /// </summary>
+        void add();
+
+        /// <summary>
+        /// Removes the component at the specified index.
+        /// The caller assumes the responsibility of providing a valid index.
+        /// </summary>
+        /// <param name="index"></param>
+        void remove(size_t index);
+
+        /// <summary>
+        /// Returns a pointer to the component at the specified index.
+        /// The caller assumes the responsibility of providing a valid index.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        void* get(size_t index);
+
+        /// <summary>
+        /// Returns the number of components stored in the buffer.
+        /// </summary>
+        /// <returns></returns>
+        size_t count() const noexcept;
+
+        /// <summary>
+        /// Returns the number of components the underlying buffer can contain.
+        /// </summary>
+        /// <returns></returns>
+        size_t capacity() const noexcept;
+
+    protected:
+
+    private:
+
+        struct Impl;
+        Core::ImplPtr<Impl, 32> m_impl;
+    };
 }
 
 #endif
