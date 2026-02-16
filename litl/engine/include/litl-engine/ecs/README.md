7c229fbcde8fc4957eb2d305ce9d28190448edda
Steven Sell <ssell@vertexfragment.com>
2026-02-15 23:51:59 -0500

    Not a whole lot ...

---
+++ litl/engine/include/litl-engine/ecs/README.md
@@ -0,0 +1,7 @@
+# LITL Entity Component System (ECS)
+
+Within here is the custom ECS implementation used by the LITL Engine.
+
+It is an archetypal implementation using 16kb chunks to store components.
+
+... todo fill me out ...
---
+++ litl/engine/include/litl-engine/ecs/archetype/chunk.hpp
@@ -0,0 +1,21 @@
+#ifndef LITL_ENGINE_ECS_ARCHETYPE_CHUNK_H__
+#define LITL_ENGINE_ECS_ARCHETYPE_CHUNK_H__
+
+namespace LITL::Engine::ECS
+{
+    /// <summary>
+    /// A 16kb block of memory which stores components for N number of entities.
+    /// The chunk represents the unit of execution for our ECS implementation.
+    /// </summary>
+    public class Chunk
+    {
+    public:
+
+    protected:
+
+    private:
+
+    };
+}
+
+#endif
