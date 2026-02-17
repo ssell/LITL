2486b958074e0c33b6a030b25b9bbb8d30abeb1a
Steven Sell <ssell@vertexfragment.com>
2026-02-17 17:32:38 -0500

    Chunk layout ordering

--- tests/src/litl-core/ecs/chunk_tests.cpp
+++ tests/src/litl-core/ecs/chunk_tests.cpp
@@ -14,11 +14,18 @@ struct Bar
 
 TEST_CASE("Chunk Layout", "[engine::ecs::chunk]")
 {
-    LITL::Engine::ECS::ChunkLayout layout;
+    LITL::Engine::ECS::ChunkLayout layoutFooBar;
+    LITL::Engine::ECS::populateChunkLayout<Foo, Bar>(&layoutFooBar);
 
-    LITL::Engine::ECS::populateChunkLayout<Foo, Bar>(&layout);
+    LITL::Engine::ECS::ChunkLayout layoutBarFoo;
+    LITL::Engine::ECS::populateChunkLayout<Bar, Foo>(&layoutBarFoo);
 
-    REQUIRE(layout.componentTypeCount == 2);
+    REQUIRE(layoutFooBar.componentTypeCount == 2);
+    REQUIRE(layoutBarFoo.componentTypeCount == 2);
+
+    // Even though they are declared in opposite orders, these should resolve to the same layout.
+    REQUIRE(layoutFooBar.componentOrder[0] == layoutBarFoo.componentOrder[0]);
+    REQUIRE(layoutFooBar.componentOrder[1] == layoutBarFoo.componentOrder[1]);
 }
 
 TEST_CASE("Header Creation", "[engine::ecs::chunk]")
@@ -26,7 +33,7 @@ TEST_CASE("Header Creation", "[engine::ecs::chunk]")
     const uint32_t capacity = 100;
 
     LITL::Engine::ECS::ChunkLayout layout;
-    layout.chunkElementCapacity = 2;
+    layout.chunkElementCapacity = capacity;
 
     LITL::Engine::ECS::Chunk chunk0(0, &layout);
     LITL::Engine::ECS::Chunk chunk1(1, &layout);
