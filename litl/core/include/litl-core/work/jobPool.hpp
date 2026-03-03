2b8dc4d2a41132431bcc6d461ff020a03c5896a8
Steven Sell <ssell@vertexfragment.com>
2026-03-03 15:29:38 -0500

    Initial impl of jobs and stuff done

--- litl/core/include/litl-core/work/jobPool.hpp
+++ litl/core/include/litl-core/work/jobPool.hpp
@@ -18,19 +18,43 @@ namespace LITL::Core
         JobPool& operator=(JobPool const&) = delete;
         ~JobPool();
 
-        [[nodiscard]] Job* createJob(JobFunc func, void* userData = nullptr) noexcept;
-        void addDependency(Job* job, Job* dependsOn) noexcept;
-        void reset() noexcept;
+        /// <summary>
+        /// Allocates a new Job.
+        /// </summary>
+        /// <param name="threadIndex"></param>
+        /// <param name="func"></param>
+        /// <param name="externalData">Pointer to externally provided data. Caller is responsible for ensuring the pointer is valid for the lifetime of the Job.</param>
+        /// <returns></returns>
+        [[nodiscard]] Job* createJob(uint32_t threadIndex, Job::JobFunc func, void* externalData = nullptr) const noexcept;
+
+        /// <summary>
+        /// Marks one job as dependent of another.
+        /// The dependent will be automatically submitted once the dependency has been run.
+        /// </summary>
+        /// <param name="dependent">The job that is dependent on another.</param>
+        /// <param name="dependency"></param>
+        /// <returns>Can return false if either Job is null or their versions do not match.</returns>
+        bool addDependency(Job* dependent, Job* dependency) const noexcept;
+
+        /// <summary>
+        /// Reset the global pool and all of the thread-local pools.
+        /// Typically called at the end of each frame.
+        /// </summary>
+        void reset() const noexcept;
+
+        /// <summary>
+        /// Returns the current version of the pool.
+        /// The version is incremented on each reset.
+        /// </summary>
+        /// <returns></returns>
+        uint32_t version() const noexcept;
 
     protected:
 
     private:
 
-        Job* allocate() noexcept;
-
         struct Impl;
         std::unique_ptr<Impl> m_pImpl;
-        static thread_local uint32_t t_threadIndex;
     };
 }
 
--- litl/core/include/litl-core/work/workScheduler.hpp
+++ litl/core/include/litl-core/work/workScheduler.hpp
@@ -7,6 +7,7 @@
 #include <vector>
 
 #include "litl-core/work/job.hpp"
+#include "litl-core/work/jobPool.hpp"
 
 namespace LITL::Core
 {
@@ -19,7 +20,126 @@ namespace LITL::Core
         WorkScheduler& operator=(WorkScheduler const&) = delete;
         ~WorkScheduler();
 
-        void submit(Job* job) noexcept;
+        /// <summary>
+        /// Allocates a Job with the provided optional external data.
+        /// 
+        /// The returned Job will not run until it is supplied to Submit.
+        /// This is to provide the caller time to hook together dependent Jobs if needed.
+        /// 
+        /// Use createAndSubmit to create a Job and immediately submit it.
+        /// </summary>
+        /// <param name="func"></param>
+        /// <param name="externalData">Pointer to externally provided data. Caller is responsible for ensuring the pointer is valid for the lifetime of the Job.</param>
+        /// <returns></returns>
+        Job* create(Job::JobFunc func, void* externalData = nullptr) noexcept;
+        
+        /// <summary>
+        /// Allocates a Job with the provided Job-local data (which will be copied) and optional external data.
+        /// 
+        /// The returned Job will not run until it is supplied to Submit.
+        /// This is to provide the caller time to hook together dependent Jobs if needed.
+        /// 
+        /// Use createAndSubmit to create a Job and immediately submit it.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="func"></param>
+        /// <param name="jobLocalData"></param>
+        /// <param name="externalData">Pointer to externally provided data. Caller is responsible for ensuring the pointer is valid for the lifetime of the Job.</param>
+        /// <returns></returns>
+        template<typename T>
+        Job* create(Job::JobFunc func, T& jobLocalData, void* externalData = nullptr) noexcept
+        {
+            static_assert(sizeof(T) <= sizeof(Job::localData));
+            static_assert(alignof(T) <= alignof(decltype(Job::localData)));
+
+            Job* job = create(func, externalData);
+            new (job->localData) T(jobLocalData);
+
+            return job;
+        }
+
+        /// <summary>
+        /// Convenience allowing for Job creation via a Lambda.
+        /// The lambda is still expected to have the Job::JobFunc signature.
+        /// </summary>
+        /// <typeparam name="F"></typeparam>
+        /// <param name="func"></param>
+        /// <param name="externalData">Pointer to externally provided data. Caller is responsible for ensuring the pointer is valid for the lifetime of the Job.</param>
+        /// <returns></returns>
+        template<typename F> requires (sizeof(F) <= 64) && std::is_trivially_destructible_v<F>
+        Job* create(F&& func, void* externalData = nullptr)
+        {
+            Job* job = create(nullptr);
+
+            // Store the lambda in the job local buffer
+            new (job->localData) std::decay_t<F>(std::forward<F>(func));
+
+            // Set the job function as invoking the lambda.
+            job->data = externalData;
+            job->func = [](Job* job, uint32_t threadIndex)
+                {
+                    auto& callable = *static_cast<std::decay_t<F>*>(job->localData);
+                    callable(job, threadIndex);
+                };
+
+            return job;
+        }
+
+        /// <summary>
+        /// Allocates a Job with the provided optional external data.
+        /// The new Job is immediately submitted to the internal scheduler to be run.
+        /// </summary>
+        /// <param name="func"></param>
+        /// <param name="externalData">Pointer to externally provided data. Caller is responsible for ensuring the pointer is valid for the lifetime of the Job.</param>
+        void createAndSubmit(Job::JobFunc func, void* externalData = nullptr) noexcept;
+
+        /// <summary>
+        /// Allocates a Job with the provided Job-local data (which will be copied) and optional external data.
+        /// The new Job is immediately submitted to the internal scheduler to be run.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="func"></param>
+        /// <param name="jobLocalData"></param>
+        /// <param name="externalData">Pointer to externally provided data. Caller is responsible for ensuring the pointer is valid for the lifetime of the Job.</param>
+        template<typename T>
+        void createAndSubmit(Job::JobFunc func, T& jobLocalData, void* externalData = nullptr) noexcept
+        {
+            submit(createAndSubmit(func, jobLocalData, externalData));
+        }
+
+        /// <summary>
+        /// Convenience allowing for Job creation via a Lambda.
+        /// The lambda is still expected to have the Job::JobFunc signature.
+        /// </summary>
+        /// <typeparam name="F"></typeparam>
+        /// <param name="func"></param>
+        /// <param name="externalData">Pointer to externally provided data. Caller is responsible for ensuring the pointer is valid for the lifetime of the Job.</param>
+        template<typename F> requires (sizeof(F) <= 64) && std::is_trivially_destructible_v<F>
+        void createAndSubmit(F&& func, void* externalData = nullptr)
+        {
+            submit(create(func, externalData));
+        }
+
+        /// <summary>
+        /// Submits a Job to be run.
+        /// There is no guarantee of when the job is run.
+        /// </summary>
+        /// <param name="job"></param>
+        void submit(Job* job) const noexcept;
+
+        /// <summary>
+        /// Marks that the specified Job is dependent on another.
+        /// 
+        /// Note that a job will be automatically submitted when it's dependency has been run.
+        /// As such, it is incorrect to list a job with dependencies and then manually submit it.
+        /// 
+        /// The dependency job (or the job they are dependent on if this is a graph situation) is
+        /// the only job that needs to be submitted.
+        /// </summary>
+        /// <param name="dependent">The job that is dependent on another.</param>
+        /// <param name="dependency"></param>
+        /// <returns>Can return false if either Job is null or their versions do not match.</returns>
+        bool addDependency(Job* dependent, Job* dependency) const noexcept;
 
     protected:
 
@@ -28,6 +148,8 @@ namespace LITL::Core
         void workerInternalLoop(uint32_t threadIndex);
 
         struct Worker;
+
+        JobPool m_pool;
         std::atomic<bool> m_running{ true };
         std::vector<std::unique_ptr<Worker>> m_workers;
         static thread_local uint32_t t_threadIndex;
