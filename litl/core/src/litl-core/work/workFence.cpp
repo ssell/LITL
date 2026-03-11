d2cb912cdd0fe127bbd5fcf550fada16f4723db7
Steven Sell <ssell@vertexfragment.com>
2026-03-11 12:04:50 -0400

    WorkFence test and fix

--- litl/core/src/litl-core/work/workFence.cpp
+++ litl/core/src/litl-core/work/workFence.cpp
@@ -9,19 +9,29 @@
 
 namespace LITL::Core
 {
+    struct WorkFence::Impl
+    {
+        JobPriority priority;
+        std::atomic<int32_t> remaining{ 0 };
+        std::binary_semaphore readySignal{ 0 };
+    };
+
     WorkFence::WorkFence()
-        : m_remaining(0)
     {
 
     }
 
     WorkFence::WorkFence(std::span<JobHandle> jobHandles)
-        : m_remaining(0)
     {
         for (auto& handle : jobHandles)
         {
             add(handle);
         }
+    }
+
+    WorkFence::~WorkFence()
+    {
+        
     }
 
     void WorkFence::add(JobHandle handle) noexcept
@@ -33,10 +43,10 @@ namespace LITL::Core
 
         handle.job->fence = this;
         
-        if (m_remaining.fetch_add(1, std::memory_order_acq_rel) == 0)
+        if (m_impl->remaining.fetch_add(1, std::memory_order_acq_rel) == 0)
         {
             // Set the fence priority to match that of the first job added.
-            m_priority = handle.job->priority;
+            m_impl->priority = handle.job->priority;
         }
     }
 
@@ -47,9 +57,9 @@ namespace LITL::Core
             return;
         }
 
-        if (m_remaining.fetch_sub(1, std::memory_order_acq_rel) == 1)
+        if (m_impl->remaining.fetch_sub(1, std::memory_order_acq_rel) == 1)
         {
-            m_readySignal.release();
+            m_impl->readySignal.release();
         }
     }
 
@@ -60,7 +70,7 @@ namespace LITL::Core
 
         bool timedOut = false;
 
-        while (m_remaining.load(std::memory_order_acquire) > 0)
+        while (m_impl->remaining.load(std::memory_order_acquire) > 0)
         {
             if ((timeoutMs > 0) && ((std::chrono::steady_clock::now() - start).count() > timeoutNs))
             {
@@ -72,7 +82,7 @@ namespace LITL::Core
             // We pull only jobs that are the same priority level as the fence (and ideally as the jobs being fenced).
             // This is to prevent the fence from grabbing and blocking on a slower low priority background job
             // when all of it's fenced jobs are higher priority fast jobs.
-            auto handle = scheduler->acquireJob(m_priority);
+            auto handle = scheduler->acquireJob(m_impl->priority);
 
             if (handle.has_value())
             {
@@ -81,7 +91,7 @@ namespace LITL::Core
             else
             {
                 // No job to run (well, our steal failed). Wait and try again.
-                std::ignore = m_readySignal.try_acquire_for(std::chrono::microseconds(50));
+                std::ignore = m_impl->readySignal.try_acquire_for(std::chrono::microseconds(50));
             }
         }
 
