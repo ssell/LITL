3ed40c261e3bfe6632c8e2769cfd01f5d09c64ec
Steven Sell <ssell@vertexfragment.com>
2026-04-18 00:22:30 -0400

    More frustum tests and operations

--- litl/core/include/litl-core/math/bounds/bounds.hpp
+++ litl/core/include/litl-core/math/bounds/bounds.hpp
@@ -9,10 +9,6 @@
 
 namespace litl::bounds
 {
-    // -------------------------------------------------------------------------------------
-    // Contains
-    // -------------------------------------------------------------------------------------
-
     /// <summary>
     /// An inclusive contains check between an axis-aligned bounding box and a point.
     /// Returns true if the point is on or within the bounds.
@@ -40,6 +36,13 @@ namespace litl::bounds
         return (bounds.center.distanceSqTo(point) <= (bounds.radius * bounds.radius));
     }
 
+    // -------------------------------------------------------------------------------------
+    // Plane
+    // -------------------------------------------------------------------------------------
+
+    // -------------------------------------------------------------------------------------
+    // Plane x Point
+
     /// <summary>
     /// An inclusive contains check to see if a point lies on, or within, a plane.
     /// A point is considered inside a plane if its signed distance is >= 0.
@@ -52,6 +55,49 @@ namespace litl::bounds
         return plane.signedDistance(point) >= 0.0f;
     }
 
+    // -------------------------------------------------------------------------------------
+    // Plane x Sphere
+
+    /// <summary>
+    /// An inclusive contains check to see if a sphere completely within the plane.
+    /// </summary>
+    /// <param name="plane"></param>
+    /// <param name="sphere"></param>
+    /// <returns></returns>
+    [[nodiscard]] constexpr bool contains(Plane plane, Sphere sphere) noexcept
+    {
+        return plane.signedDistance(sphere.center) >= sphere.radius;
+    }
+
+    /// <summary>
+    /// Calculates it the sphere straddles the plane - neither fully within or outside.
+    /// </summary>
+    /// <param name="plane"></param>
+    /// <param name="sphere"></param>
+    /// <returns></returns>
+    [[nodiscard]] constexpr bool intersects(Plane plane, Sphere sphere) noexcept
+    {
+        return abs(plane.signedDistance(sphere.center)) < sphere.radius;
+    }
+
+    /// <summary>
+    /// Calculates it the sphere lies completely outside the plane.
+    /// </summary>
+    /// <param name="plane"></param>
+    /// <param name="sphere"></param>
+    /// <returns></returns>
+    [[nodiscard]] constexpr bool isOutside(Plane plane, Sphere sphere) noexcept
+    {
+        return plane.signedDistance(sphere.center) < -sphere.radius;
+    }
+
+    // -------------------------------------------------------------------------------------
+    // Frustum
+    // -------------------------------------------------------------------------------------
+
+    // -------------------------------------------------------------------------------------
+    // Frustum x Point
+
     /// <summary>
     /// Calculates the rejection bitmask (outcode) for the given point and frustum.
     /// Each set bit corresponds to a plane that the point is outside of.
@@ -61,8 +107,6 @@ namespace litl::bounds
     /// <returns></returns>
     [[nodiscard]] constexpr uint32_t containsMask(Frustum const& frustum, vec3 point) noexcept
     {
-        uint32_t rejectionMask = 0;
-
         /**
          * Iterate all sides, setting their corresponding bit mask value to 1 if they were rejected.
          *
@@ -74,6 +118,8 @@ namespace litl::bounds
          *
          * See Frustum::Sides
          */
+
+        uint32_t rejectionMask = 0;
 
         for (uint32_t i = 0u; i < frustum.sideCount(); ++i)
         {
@@ -93,6 +139,138 @@ namespace litl::bounds
     [[nodiscard]] constexpr bool contains(Frustum const& frustum, vec3 point) noexcept
     {
         return containsMask(frustum, point) == 0;
+    }
+
+    // -------------------------------------------------------------------------------------
+    // Frustum x Sphere
+
+    /// <summary>
+    /// An inclusive contains check between a frustum and a sphere.
+    /// A sphere is contained within a frustum if it is also contained within all of its planes.
+    /// </summary>
+    /// <param name="frustum"></param>
+    /// <param name="sphere"></param>
+    /// <returns></returns>
+    [[nodiscard]] constexpr bool contains(Frustum const& frustum, Sphere sphere) noexcept
+    {
+        bool allContains = true;
+
+        for (uint32_t i = 0u; i < frustum.sideCount(); ++i)
+        {
+            allContains = allContains && contains(frustum.getSide(static_cast<Frustum::Side>(i)), sphere);
+        }
+
+        return allContains;
+    }
+
+    /// <summary>
+    /// A sphere intersects with a frustum if it is not fully outside any single plane.
+    /// </summary>
+    /// <param name="frustum"></param>
+    /// <param name="sphere"></param>
+    /// <returns></returns>
+    [[nodiscard]] constexpr bool intersects(Frustum const& frustum, Sphere sphere) noexcept
+    {
+        bool allIntersects = true;
+
+        for (uint32_t i = 0u; i < frustum.sideCount(); ++i)
+        {
+            allIntersects = allIntersects && !isOutside(frustum.getSide(static_cast<Frustum::Side>(i)), sphere);
+        }
+
+        return allIntersects;
+    }
+
+    // -------------------------------------------------------------------------------------
+    // Frustum Intersection Classification
+
+    struct FrustumClassification
+    {
+        enum Type
+        {
+            /// <summary>
+            /// The object is fully contained within all frustum planes.
+            /// </summary>
+            Inside = 0,
+
+            /// <summary>
+            /// The object is not fully outside any plane, but intersects at least one frustum plane.
+            /// </summary>
+            Intersects,
+
+            /// <summary>
+            /// The object is fully outside at least one frustum plane.
+            /// </summary>
+            Outside
+        };
+
+        /// <summary>
+        /// Each bit correlates to a frustum plane that the object is fully outside of.
+        /// </summary>
+        uint32_t outsideMask{ 0 };
+
+        /// <summary>
+        /// Each bit correlates to a frustum plane that the object straddles/intersects.
+        /// </summary>
+        uint32_t straddleMask{ 0 };
+
+        [[nodiscard]] constexpr Type type() const noexcept
+        {
+            if (outsideMask != 0)
+            {
+                return Outside;
+            }
+            else if (straddleMask != 0)
+            {
+                return Intersects;
+            }
+            else
+            {
+                return Inside;
+            }
+        }
+    };
+
+    /// <summary>
+    /// Classifies how the sphere intersects (or doesn't) with the frustum.<br/>
+    /// 
+    /// The active mask, if specified, is the parent's (straddleMask | outsideMask) which is used
+    /// to skip planes that are we already know completely contain the parent (and thus) the child.
+    /// </summary>
+    /// <param name="frustum"></param>
+    /// <param name="sphere"></param>
+    /// <param name="activeMask"></param>
+    /// <returns></returns>
+    [[nodiscard]] constexpr FrustumClassification classify(Frustum const& frustum, Sphere sphere, uint32_t activeMask = 0b111111) noexcept
+    {
+        FrustumClassification result{};
+
+        for (uint32_t i = 0u; i < frustum.sideCount(); ++i)
+        {
+            const uint32_t maskBit = 1u << i;
+
+            if ((activeMask & maskBit) == 0u)
+            {
+                // the plane fully contains the parent, and thus this child (straddleBit | outsideBit = 0)
+                continue;
+            }
+
+            const float dist = frustum.getSide(static_cast<Frustum::Side>(i)).signedDistance(sphere.center);
+
+            if (dist < -sphere.radius)
+            {
+                // once the sphere is fully outside a single plane then it is classified as outside.
+                result.outsideMask |= maskBit;
+                return result;
+            }
+            
+            if (dist <= sphere.radius)
+            {
+                result.straddleMask |= maskBit;
+            }
+        }
+
+        return result;
     }
 }
 
