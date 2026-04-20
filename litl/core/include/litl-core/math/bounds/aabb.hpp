d493db83b285ce75a2c74d96e10cf8819638036a
Steven Sell <ssell@vertexfragment.com>
2026-04-19 23:34:42 -0400

    AABB p/n-vertex

--- litl/core/include/litl-core/math/bounds/aabb.hpp
+++ litl/core/include/litl-core/math/bounds/aabb.hpp
@@ -17,19 +17,55 @@ namespace litl::bounds
         vec3 min{ 0.0f, 0.0f, 0.0f };
         vec3 max{ 0.0f, 0.0f, 0.0f };
 
+        /// <summary>
+        /// Returns the distance between the min/max points.
+        /// </summary>
+        /// <returns></returns>
         [[nodiscard]] constexpr vec3 extents() const noexcept
         {
             return (max - min);
         }
 
+        /// <summary>
+        /// Returns the distance between the center point and the min/max points.
+        /// </summary>
+        /// <returns></returns>
         [[nodiscard]] constexpr vec3 halfExtents() const noexcept
         {
             return (max - min) * 0.5f;
         }
 
+        /// <summary>
+        /// Returns the center point between the min/max points.
+        /// </summary>
+        /// <returns></returns>
         [[nodiscard]] constexpr vec3 center() const noexcept
         {
             return (min + ((max - min) * 0.5f));
+        }
+
+        /// <summary>
+        /// Given a normal, returns the p-vertex (positive vertex) which is the corner point that lies furthest along it.
+        /// </summary>
+        /// <param name="normal"></param>
+        /// <returns></returns>
+        [[nodiscard]] constexpr vec3 pVertex(vec3 normal) const noexcept
+        {
+            return vec3{
+                normal.x() >= 0.0f ? max.x() : min.x(),
+                normal.y() >= 0.0f ? max.y() : min.y(),
+                normal.z() >= 0.0f ? max.z() : min.z()
+            };
+        }
+
+        /// <summary>
+        /// Given a normal, returns the n-vertex (negative vertex) which is the corner point that lies furthest along the negative of the normal.
+        /// </summary>
+        /// <param name="normal"></param>
+        /// <returns></returns>
+        [[nodiscard]] constexpr vec3 nVertex(vec3 normal) const noexcept
+        {
+            return pVertex(-normal);
         }
 
         [[nodiscard]] static constexpr AABB fromMinMax(vec3 min, vec3 max) noexcept
