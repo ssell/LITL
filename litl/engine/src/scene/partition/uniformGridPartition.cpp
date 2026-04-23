2b2ac8bcb851570e1b6229cc3abb165f56c230d7
Steven Sell <ssell@vertexfragment.com>
2026-04-23 16:20:09 -0400

    Done with initial uniform grid implementation

--- litl/engine/src/scene/partition/uniformGridPartition.cpp
+++ litl/engine/src/scene/partition/uniformGridPartition.cpp
@@ -6,75 +6,13 @@ namespace litl
 {
     struct GridCell
     {
-        std::unordered_map<EntityId, uint32_t> entityIndices;
-        std::vector<Entity> entityEntries;
+        std::vector<Entity> entities;
         std::vector<bounds::AABB> entityBounds;
 
         GridCell(float x, float z, float size, float yMin, float yMax)
-            : cellBounds(bounds::AABB::fromMinMax(vec3{x, yMin, z}, vec3{x + size, yMax, z + size}))
+            : cellBounds(bounds::AABB::fromMinMax(vec3{ x, yMin, z }, vec3{ x + size, yMax, z + size }))
         {
 
-        }
-
-        void add(Entity entity, bounds::AABB bounds) noexcept
-        {
-            auto find = entityIndices.find(entity.index);
-            uint32_t index = 0;
-
-            if (find != entityIndices.end())
-            {
-                // entity already exists in the graph
-                index = find->second;
-                Entity prevEntity = entityEntries[index];
-
-                if (entity.index <= prevEntity.index)
-                {
-                    // Same exact entity or an older version of it. Do nothing.
-                    return;
-                }
-                else
-                {
-                    // Newer version of the entity. Do nothing and overwrite it below.
-                }
-            }
-            else
-            {
-                index = entityEntries.size();
-            }
-
-            entityIndices[entity.index] = index;
-            
-            if (entityEntries.size() > index)
-            {
-                entityEntries[index] = entity;
-                entityBounds[index] = bounds;
-            }
-            else
-            {
-                entityEntries.push_back(entity);
-                entityBounds.push_back(bounds);
-            }
-        }
-
-        void remove(Entity entity) noexcept
-        {
-            auto find = entityIndices.find(entity.index);
-
-            if (find != entityIndices.end())
-            {
-                // Swap with the entity at the end and then pop the back
-                const uint32_t index = find->second;
-                const uint32_t lastIndex = entityEntries.size() - 1;
-                const Entity lastEntity = entityEntries[lastIndex];
-
-                entityIndices[lastEntity.index] = index;
-                entityEntries[index] = entityEntries[lastIndex];
-                entityBounds[index] = entityBounds[lastIndex];
-
-                entityIndices.erase(find);
-                entityEntries.pop_back();
-                entityBounds.pop_back();
-            }
         }
 
         void query(bounds::AABB aabb, std::vector<Entity>& entities) const noexcept
@@ -83,23 +21,23 @@ namespace litl
 
             switch (intersection)
             {
-            // The cell is completely inside the AABB, so add all
+                // The cell is completely inside the AABB, so add all
             case bounds::IntersectionType::Inside:
                 addAllTo(entities);
                 break;
 
-            // The cell intersects the AABB, so add some
+                // The cell intersects the AABB, so add some
             case bounds::IntersectionType::Intersects:
                 for (uint32_t i = 0u; i < count(); ++i)
                 {
                     if (bounds::intersects(aabb, entityBounds[i]))      // intersects returns true for both true intersection (straddle) and containment
                     {
-                        entities.push_back(entityEntries[i]);
+                        entities.push_back(entities[i]);
                     }
                 }
                 break;
 
-            // The cell is completely outside the AABB, so add none
+                // The cell is completely outside the AABB, so add none
             case bounds::IntersectionType::Outside:
             default:
                 break;
@@ -123,7 +61,7 @@ namespace litl
                 {
                     if (bounds::intersects(sphere, entityBounds[i]))        // intersects returns true for both true intersection (straddle) and containment
                     {
-                        entities.push_back(entityEntries[i]);
+                        entities.push_back(entities[i]);
                     }
                 }
                 break;
@@ -152,7 +90,7 @@ namespace litl
                 {
                     if (bounds::intersects(frustum, entityBounds[i]))       // intersects returns true for both true intersection (straddle) and containment
                     {
-                        entities.push_back(entityEntries[i]);
+                        entities.push_back(entities[i]);
                     }
                 }
                 break;
@@ -166,14 +104,14 @@ namespace litl
 
         [[nodiscard]] uint32_t count() const noexcept
         {
-            return static_cast<uint32_t>(entityEntries.size());
+            return static_cast<uint32_t>(entities.size());
         }
 
     private:
 
         void addAllTo(std::vector<Entity>& entities) const noexcept
         {
-            for (Entity entity : entityEntries)
+            for (Entity const& entity : entities)
             {
                 entities.push_back(entity);
             }
@@ -195,6 +133,16 @@ namespace litl
         /// </summary>
         std::vector<GridCell> cells;
 
+        /// <summary>
+        /// Maps an entity to the GridCell it is in.
+        /// </summary>
+        std::unordered_map<EntityId, uint32_t> entityToCell;
+
+        /// <summary>
+        /// Maps an entity to the slot within the GridCell it s in.
+        /// </summary>
+        std::unordered_map<EntityId, uint32_t> entityToCellSlot;
+
         [[nodiscard]] uint32_t getGridLocalX(float worldX) const noexcept
         {
             return clamp(static_cast<uint32_t>(worldX), 0u, options.cellCount - 1u);
@@ -215,6 +163,130 @@ namespace litl
             const auto gridLocalX = getGridLocalX(point.x());
             const auto gridLocalZ = getGridLocalZ(point.z());
             return (gridLocalX + (gridLocalZ * options.cellCount));
+        }
+
+        void add(Entity entity, bounds::AABB bounds)
+        {
+            LITL_ASSERT_MSG(entityToCell.find(entity.index) != entityToCell.end(), "Attempting to add Entity to UniformGridPartition whose index is already tracked.", );
+
+            const uint32_t cellIndex = getIndex(bounds.center());
+            addEntityTo(entity, bounds, cellIndex);
+        }
+
+        void remove(Entity entity)
+        {
+            const auto findEntity = entityToCell.find(entity.index);
+
+            if (findEntity == entityToCell.end())
+            {
+                return;
+            }
+
+            const auto cellIndex = findEntity->second;
+            const auto cellSlot = entityToCellSlot.find(cellIndex)->second;
+
+            auto& cell = cells[cellIndex];
+
+            if (cell.entities[cellSlot].index > entity.index)
+            {
+                // The entity tracked is newer than the one being requested to remove.
+                return;
+            }
+
+            removeEntityFrom(cellIndex, cellSlot);
+        }
+
+        void update(Entity entity, bounds::AABB bounds)
+        {
+            const auto findEntity = entityToCell.find(entity.index);
+
+            if (findEntity == entityToCell.end())
+            {
+                return;
+            }
+
+            const auto prevEntityIndex = findEntity->second;
+            const auto prevEntitySlot = entityToCellSlot.find(entity.index)->second;
+            const auto currEntityIndex = getIndex(bounds.center());
+
+            if (currEntityIndex == prevEntityIndex)
+            {
+                // The entity is in the same cell. Just update the bounds.
+                cells[prevEntityIndex].entityBounds[prevEntitySlot] = bounds;
+            }
+            else
+            {
+                // The entity has moved cells. Remove from its current cell and add to the new one.
+                removeEntityFrom(prevEntityIndex, prevEntitySlot);
+                addEntityTo(entity, bounds, currEntityIndex);
+            }
+        }
+
+        void query(bounds::AABB aabb, std::vector<Entity>& entities) const noexcept
+        {
+            const uint32_t startX = getGridLocalX(aabb.min.x());
+            const uint32_t endX = getGridLocalX(aabb.max.x());
+
+            const uint32_t startZ = getGridLocalZ(aabb.min.z());
+            const uint32_t endZ = getGridLocalZ(aabb.max.z());
+
+            for (uint32_t z = startZ; z <= endZ; ++z)
+            {
+                for (uint32_t x = startX; x <= endX; ++x)
+                {
+                    cells[x + (z * options.cellCount)].query(aabb, entities);
+                }
+            }
+        }
+
+        void query(bounds::Sphere sphere, std::vector<Entity>& entities) const noexcept
+        {
+            const uint32_t startX = getGridLocalX(sphere.center.x() - sphere.radius);
+            const uint32_t endX = getGridLocalX(sphere.center.x() + sphere.radius);
+
+            const uint32_t startZ = getGridLocalZ(sphere.center.z() - sphere.radius);
+            const uint32_t endZ = getGridLocalZ(sphere.center.z() + sphere.radius);
+
+            for (uint32_t z = startZ; z <= endZ; ++z)
+            {
+                for (uint32_t x = startX; x <= endX; ++x)
+                {
+                    cells[x + (z * options.cellCount)].query(sphere, entities);
+                }
+            }
+        }
+
+        void query(bounds::Frustum const& frustum, std::vector<Entity>& entities) const noexcept
+        {
+            for (auto& cell : cells)
+            {
+                cell.query(frustum, entities);
+            }
+        }
+
+    private:
+
+        void addEntityTo(Entity entity, bounds::AABB bounds, uint32_t cellIndex) noexcept
+        {
+            entityToCell[entity.index] = cellIndex;
+            entityToCellSlot[entity.index] = cells[cellIndex].entities.size();
+            cells[cellIndex].entities.push_back(entity);
+            cells[cellIndex].entityBounds.push_back(bounds);
+        }
+
+        void removeEntityFrom(uint32_t cellIndex, uint32_t cellSlot) noexcept
+        {
+            auto& cell = cells[cellIndex];
+            const auto swappedEntity = cell.entities.back();
+            const auto swappedAABB = cell.entityBounds.back();
+
+            cell.entities[cellSlot] = swappedEntity;
+            cell.entityBounds[cellSlot] = swappedAABB;
+            entityToCell[swappedEntity.index] = cellIndex;
+            entityToCellSlot[swappedEntity.index] = cellSlot;
+
+            cell.entities.pop_back();
+            cell.entityBounds.pop_back();
         }
     };
 
@@ -241,72 +313,34 @@ namespace litl
         }
     }
 
-    UniformGridPartition::~UniformGridPartition()
-    {
-
-    }
-
     void UniformGridPartition::add(Entity entity, bounds::AABB bounds) noexcept
     {
-        const auto gridIndex = m_impl->getIndex(bounds.center());
-        m_impl->cells[gridIndex].add(entity, bounds);
+        m_impl->add(entity, bounds);
     }
 
-    void UniformGridPartition::remove(Entity entity, bounds::AABB bounds) noexcept
+    void UniformGridPartition::remove(Entity entity) noexcept
     {
-        const auto gridIndex = m_impl->getIndex(bounds.center());
-        m_impl->cells[gridIndex].remove(entity);
+        m_impl->remove(entity);
     }
 
-    void UniformGridPartition::update(Entity entity, bounds::AABB prev, bounds::AABB curr) noexcept
+    void UniformGridPartition::update(Entity entity, bounds::AABB bounds) noexcept
     {
-        const auto prevIndex = m_impl->getIndex(prev.center());
-        const auto currIndex = m_impl->getIndex(curr.center());
-
-        if (prevIndex != currIndex)
-        {
-            m_impl->cells[prevIndex].remove(entity);
-            m_impl->cells[currIndex].add(entity, curr);
-        }
+        m_impl->update(entity, bounds);
     }
 
     void UniformGridPartition::query(bounds::AABB aabb, std::vector<Entity>& entities) const noexcept
     {
-        const uint32_t startX = m_impl->getGridLocalX(aabb.min.x());
-        const uint32_t endX = m_impl->getGridLocalX(aabb.max.x());
-
-        const uint32_t startZ = m_impl->getGridLocalZ(aabb.min.z());
-        const uint32_t endZ = m_impl->getGridLocalZ(aabb.max.z());
-
-        for (uint32_t z = startZ; z <= endZ; ++z)
-        {
-            for (uint32_t x = startX; x <= endX; ++x)
-            {
-                m_impl->cells[x + (z * m_impl->options.cellCount)].query(aabb, entities);
-            }
-        }
+        m_impl->query(aabb, entities);
     }
 
     void UniformGridPartition::query(bounds::Sphere sphere, std::vector<Entity>& entities) const noexcept
     {
-        const uint32_t startX = m_impl->getGridLocalX(sphere.center.x() - sphere.radius);
-        const uint32_t endX = m_impl->getGridLocalX(sphere.center.x() + sphere.radius);
-
-        const uint32_t startZ = m_impl->getGridLocalZ(sphere.center.z() - sphere.radius);
-        const uint32_t endZ = m_impl->getGridLocalZ(sphere.center.z() + sphere.radius);
-
-        for (uint32_t z = startZ; z <= endZ; ++z)
-        {
-            for (uint32_t x = startX; x <= endX; ++x)
-            {
-                m_impl->cells[x + (z * m_impl->options.cellCount)].query(sphere, entities);
-            }
-        }
+        m_impl->query(sphere, entities);
     }
 
     void UniformGridPartition::query(bounds::Frustum const& frustum, std::vector<Entity>& entities) const noexcept
     {
-
+        m_impl->query(frustum, entities);
     }
 
     uint32_t UniformGridPartition::getCellSize() const noexcept
