847f28eada1a85b29788a0f8653f8413ffbd69bb
Steven Sell <ssell@vertexfragment.com>
2026-04-30 00:36:23 -0400

    Start on SceneCommandProcessor

---
+++ litl/engine/include/litl-engine/scene/sceneCommandProcessor.hpp
@@ -0,0 +1,32 @@
+#ifndef LITL_ENGINE_SCENE_COMMAND_PROCESSOR_H__
+#define LITL_ENGINE_SCENE_COMMAND_PROCESSOR_H__
+
+#include <memory>
+#include <span>
+#include <vector>
+
+#include "litl-ecs/entity/entitySceneCommand.hpp"
+#include "litl-engine/scene/scene.hpp"
+
+namespace litl
+{
+    /// <summary>
+    /// Processes a list of EntitySceneCommands output by the SceneManager into a specified Scene.
+    /// </summary>
+    class SceneCommandProcessor
+    {
+    public:
+
+        void process(std::shared_ptr<Scene> scene, std::span<EntitySceneCommand const> sceneCommands) noexcept;
+
+    protected:
+
+    private:
+
+        void sortCommands(std::span<EntitySceneCommand const> sceneCommands) noexcept;
+
+        std::vector<EntitySceneCommand> m_sortedCommands;
+    };
+}
+
+#endif
