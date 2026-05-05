b700455e1bbabfaa402fcec84f95c218c508e377
Steven Sell <ssell@vertexfragment.com>
2026-05-05 12:42:06 -0400

    Progress on change over to changes

--- litl/engine/src/scene/sceneCommandProcessor.cpp
+++ litl/engine/src/scene/sceneCommandProcessor.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 
+#include "litl-ecs/archetype/archetypeRegistry.hpp"
 #include "litl-engine/scene/sceneCommandProcessor.hpp"
 #include "litl-engine/ecs/components/transform.hpp"
 #include "litl-engine/ecs/components/bounds.hpp"
@@ -7,11 +8,119 @@
 namespace litl
 {
 
-    void SceneCommandProcessor::process(Scene& scene, World& world, std::span<EntityCommand const> entityCommands) noexcept
+    void SceneCommandProcessor::process(Scene& scene, World& world, std::span<EntityChange const> entityChanges) noexcept
     {
-        compileSceneCommands(entityCommands);
+        if (entityChanges.size() == 0)
+        {
+            return;
+        }
+
+        sortCommands(entityChanges);
+
+        // The logic below is dependent on the EntityCommandProcessor performing deduplication and remove non-SetParent commands
+        // when an entity is also being destroyed at the same time. This allows the logic below to avoid having to perform checks itself.
+
+        for (auto& change : entityChanges)
+        {
+            switch (change.type)
+            {
+            case EntityChangeType::CreateEntity:
+                // Create on its own does not impact the scene. A newly created entity has no components and thus can not be tracked.
+                break;
+
+            case EntityChangeType::DestroyEntity:
+                onDestroyEntity(scene, world, change);
+                break;
+
+            case EntityChangeType::ChangeArchetype:
+                onChangeArchetype(scene, world, change);
+                break;
+
+            case EntityChangeType::SetParent:
+                onSetParent(scene, world, change);
+                break;
+
+            default:
+                break;
+            }
+        }
     }
 
+    void SceneCommandProcessor::sortCommands(std::span<EntityChange const> entityChanges) noexcept
+    {
+        m_sortedChanges.clear();
+        m_sortedChanges.reserve(entityChanges.size());
+        m_sortedChanges.assign(entityChanges.begin(), entityChanges.end());
+
+        std::ranges::stable_sort(m_sortedChanges, [](EntityChange a, EntityChange b)
+        {
+            // SetParent commands go last, everything else groups by entity
+            const bool aIsSetParent = a.type == EntityChangeType::SetParent;
+            const bool bIsSetParent = b.type == EntityChangeType::SetParent;
+
+            // Create a tuple of references. Tuple comparison is lexicographic, so it compares SetParent first then entity and then command type.
+            return std::tie(aIsSetParent, a.entity, a.type) < std::tie(bIsSetParent, b.entity, b.type);
+        });
+    }
+
+    void SceneCommandProcessor::onDestroyEntity(Scene& scene, World& world, EntityChange const& change) const noexcept
+    {
+        auto* archetype = ArchetypeRegistry::getById(change.prevArchetype);
+
+        if (archetype->hasComponent<Transform>())
+        {
+            scene.remove(change.entity);
+        }
+    }
+
+    void SceneCommandProcessor::onChangeArchetype(Scene& scene, World& world, EntityChange const& change) const noexcept
+    {
+        if (change.prevArchetype == change.currArchetype)
+        {
+            return;
+        }
+
+        auto* prevArchetype = ArchetypeRegistry::getById(change.prevArchetype);
+        auto* currArchetype = ArchetypeRegistry::getById(change.currArchetype);
+
+        bool prevHadTransform = prevArchetype->hasComponent<Transform>();
+        bool prevHadBounds = prevArchetype->hasComponent<Bounds>();
+
+        bool currHasTransform = currArchetype->hasComponent<Transform>();
+        bool currHasBounds = currArchetype->hasComponent<Bounds>();
+
+        if (prevHadTransform && !currHasTransform)
+        {
+            // Lost the Transform component. Can no longer be tracked.
+            scene.remove(change.entity);
+        }
+        else if (prevHadBounds && !currHasBounds)
+        {
+            // Lost the Bounds component. Need to inform the Scene Partition.
+            // ... todo ...
+        }
+        else if (!prevHadTransform && currHasTransform)
+        {
+            auto record = world.getEntityRecord(change.entity);
+
+            // Gained the Transform component. Start tracking it.
+            if (currHasBounds)
+            {
+                scene.add(change.entity, currArchetype->getComponent<Transform>(record), {});
+            }
+            else
+            {
+
+            }
+        }
+    }
+
+    void SceneCommandProcessor::onSetParent(Scene& scene, World& world, EntityChange const& change) const noexcept
+    {
+        // ... todo ..
+    }
+
+    /*
     void SceneCommandProcessor::compileSceneCommands(std::span<EntityCommand const> entityCommands) noexcept
     {
         m_allCommands.clear();
@@ -61,7 +170,6 @@ namespace litl
 
     }
 
-    /*
         if (sceneCommands.empty())
         {
             return;
