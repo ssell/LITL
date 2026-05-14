f7ba505f29f68b79fd019628f08a7355218fc79c
Steven Sell <ssell@vertexfragment.com>
2026-05-14 15:26:47 -0400

    Handle pool

--- litl/core/include/litl-core/handles.hpp
+++ litl/core/include/litl-core/handles.hpp
@@ -1,14 +1,122 @@
 #ifndef LITL_CORE_HANDLES_H__
 #define LITL_CORE_HANDLES_H__
 
+#include <cstdint>
+#include <vector>
+
 using LITLHandle = void*;
 
+// OLD STYLE - to replace
 namespace litl
 {
 #define DEFINE_LITL_HANDLE(Name) struct Name { LITLHandle handle; }
 #define LITL_PACK_HANDLE(HandleType, HandleObj) HandleType { HandleObj }
 #define LITL_UNPACK_HANDLE(HandleType, HandleObj) static_cast<HandleType*>(HandleObj.handle)
 #define LITL_UNPACK_HANDLE_PTR(HandleType, HandlePtr) static_cast<HandleType*>(HandlePtr->handle)
+}
+
+// NEW STYLE
+namespace litl
+{
+    template<typename Tag>
+    struct Handle
+    {
+        uint32_t index;
+        uint32_t version;
+
+        bool operator==(Handle const&) const noexcept = default;
+        bool isValid() const noexcept { return version != 0; }
+    };
+
+    /// <summary>
+    /// A generic handle pool.
+    /// 
+    /// Used by various backends to manage their opaque handles.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="Tag"></typeparam>
+    template<typename T, typename Tag>
+    class HandlePool
+    {
+    public:
+
+        /// <summary>
+        /// Create and returns a new opaque handle for the given payload.
+        /// </summary>
+        /// <param name="payload"></param>
+        /// <returns></returns>
+        [[nodiscard]] Handle<Tag> create(T payload) noexcept
+        {
+            uint32_t index = 0;
+
+            if (!m_freeList.empty())
+            {
+                index = m_freeList.back();
+                m_freeList.pop_back();
+            }
+            else
+            {
+                index = static_cast<uint32_t>(m_slots.size());
+                m_slots.emplace_back();
+            }
+
+            m_slots[index].version++;
+            m_slots[index].payload = std::move(payload);
+
+            return Handle<Tag>{ index, m_slots[index].version };
+        }
+
+        /// <summary>
+        /// Retrieves the payload tied to the provided handle.
+        /// If the handle is out-of-date, or no such payload was found, then returns nullptr.
+        /// </summary>
+        /// <param name="handle"></param>
+        /// <returns></returns>
+        [[nodiscard]] T* get(Handle<Tag> handle) noexcept
+        {
+            if (!valid(handle))
+            {
+                return nullptr;
+            }
+
+            return &m_slots[handle.index].payload;
+        }
+
+        /// <summary>
+        /// Destroys (invalidates) the provided handle if it is currently valid.
+        /// </summary>
+        /// <param name="handle"></param>
+        void destroy(Handle<Tag> handle) noexcept
+        {
+            if (!valid(handle))
+            {
+                return;
+            }
+
+            m_slots[handle.index].version++;    // invalidate the the old handle by bumping the version
+            m_slots[handle.index].payload = T{};
+            m_freeList.push_back(handle.index);
+        }
+
+        [[nodiscard]] bool valid(Handle<Tag> handle) const noexcept
+        {
+            return (handle.index < m_slots.size()) && (handle.version == m_slots[handle.index].version);
+        }
+
+    private:
+
+        struct Slot
+        {
+            uint32_t version;
+            T payload;
+        };
+
+        std::vector<Slot> m_slots;
+        std::vector<uint32_t> m_freeList;
+    };
+
+    struct PipelineTag {};
+    using PipelineHandle = Handle<PipelineTag>;
 }
 
 #endif
