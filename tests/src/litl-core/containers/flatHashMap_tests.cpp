1bc867613ff4df92f55f25133e72916324b7f746
Steven Sell <ssell@vertexfragment.com>
2026-05-26 11:37:38 -0400

    String Key Map Performance Test

--- tests/src/litl-core/containers/flatHashMap_tests.cpp
+++ tests/src/litl-core/containers/flatHashMap_tests.cpp
@@ -1,4 +1,6 @@
 #include "tests.hpp"
+
+#include <array>
 #include <cstdint>
 
 #include "litl-core/containers/flatHashMap.hpp"
@@ -81,4 +83,86 @@ namespace litl::tests
         REQUIRE(map.contains(2) == false);
         REQUIRE(map.contains(3) == false);
     } LITL_END_TEST_CASE
+
+    LITL_TEST_CASE("String key", "[core::containers::flatHashMap]")
+    {
+        FlatHashMap<std::string, uint32_t> map{};
+
+        map.insert("Hello", 55);
+        map.insert("World!", 1337);
+
+        REQUIRE(map.size() == 2);
+
+        REQUIRE(map.contains("Hello") == true);
+        REQUIRE(map.contains("hello") == false);
+        REQUIRE(map.contains("World!") == true);
+        REQUIRE(map.contains("Hello, World!") == false);
+    } LITL_END_TEST_CASE
+
+/*
+    LITL_TEST_CASE("String Key Map Performance Test", "[core::containers::flatHashMap]")
+    {
+        //   ~5 values: vector beats map
+        //  ~10 values: vector matches map
+        // ~100 values: map beats vector
+
+        std::array<std::string, 5> values = { "string", "mammoth", "sordid", "faithful", "fence" }; // uncomment for 10, "coherent", "puzzling", "ludicrous", "size", "trot" }; // uncomment for 50 total , "daily", "heap", "direful", "walk", "awake", "current", "imported", "terrific", "aloof", "arrive", "fluttering", "nine", "joke", "collar", "addicted", "understood", "familiar", "cheer", "exultant", "harm", "toothpaste", "groovy", "loving", "bells", "road", "glossy", "stream", "substantial", "decorate", "cheat", "beautiful", "relax", "living", "breath", "grab", "abandoned", "hospitable", "pathetic", "combative", "obeisant" };
+        std::array<std::string, 100> search = { "size", "understood", "relieved", "entertaining", "taste", "glossy", "decorate", "trot", "material", "meddle", "garrulous", "relax", "hapless", "exultant", "mammoth", "island", "pathetic", "walk", "tearful", "knock", "coherent", "flawless", "road", "shave", "cheer", "puzzling", "fluttering", "trot", "awake", "lovely", "grab", "collar", "cloth", "trains", "arrive", "chin", "toothpaste", "blush", "development", "fold", "loving", "current", "stream", "bells", "furniture", "fence", "living", "obeisant", "roasted", "foolish", "sordid", "boy", "kaput", "joke", "unnatural", "direful", "ludicrous", "substantial", "breath", "cheat", "chew", "airplane", "yak", "proud", "committee", "beautiful", "pizzas", "faithful", "soap", "combative", "grade", "furtive", "harm", "lie", "bright", "economic", "dust", "nappy", "string", "ruddy", "sweltering", "aloof", "cake", "familiar", "toys", "imported", "daily", "trace", "miniature", "pass", "addicted", "jolly", "heap", "hospitable", "abandoned", "business", "terrific", "nine", "present", "groovy" };
+
+        FlatHashMap<std::string, uint32_t> map{};
+        std::vector<std::string> vec;
+
+        for (auto& value : values)
+        {
+            map.insert(value, 1);
+            vec.push_back(value);
+        }
+
+
+        // warm up
+        auto foundCount = 0;
+        for (auto i = 0; i < 10000; ++i) { if (map.contains(search[i % 100])) { foundCount++; } }
+
+        // map perf
+        foundCount = 0;
+        auto startMap = std::chrono::steady_clock::now();
+
+        for (auto i = 0; i < 100000; ++i)
+        {
+            if (map.contains(search[i % 100]))
+            {
+                foundCount++;
+            }
+        }
+
+        auto endMap = std::chrono::steady_clock::now();
+
+        // vec perf
+        foundCount = 0;
+        auto startVec = std::chrono::steady_clock::now();
+
+        for (auto i = 0; i < 100000; ++i)
+        {
+            auto& value = search[i % 100];
+
+            for (auto& vecValue : vec)
+            {
+                if (vecValue == value)
+                {
+                    foundCount++;
+                    break;
+                }
+            }
+        }
+
+        auto endVec = std::chrono::steady_clock::now();
+
+        // compare
+        auto mapPerfSec = std::chrono::duration<float>(endMap - startMap).count();
+        auto vecPerfSec = std::chrono::duration<float>(endVec - startVec).count();
+
+        REQUIRE(vecPerfSec > mapPerfSec);
+
+    } LITL_END_TEST_CASE
+*/
 }
