706dfef407376217d8ec84efc376174c0c4ad3fe
Steven Sell <ssell@vertexfragment.com>
2026-07-24 12:04:49 -0400

    Start on basic file utility

---
+++ litl/core/src/litl-core/file.cpp
@@ -0,0 +1,66 @@
+#include <fstream>
+
+#include "litl-core/file.hpp"
+#include "litl-core/logging/logging.hpp"
+
+namespace litl
+{
+    File::File(std::string const& path)
+    {
+        m_file = path;
+    }
+
+    std::string File::localPath() const noexcept
+    {
+        return m_file.relative_path().string();
+    }
+
+    std::string File::absolutePath() const noexcept
+    {
+        return std::filesystem::canonical(m_file).string();
+    }
+
+    std::optional<bool> File::exists() const noexcept
+    {
+        std::error_code error;
+        bool exists = std::filesystem::exists(m_file, error);
+
+        if (!error)
+        {
+            return exists;
+        }
+
+        logWarning("Attempt to check if the file at '", m_file.string(), "' exists failed with error code ", error.value());
+
+        return std::nullopt;
+    }
+
+    std::optional<std::vector<std::byte>> File::readAllBytes() const noexcept
+    {
+        if (!exists().has_value())
+        {
+            logWarning("Attempt read bytes for file at '", m_file.string(), "' failed as it does not exist.");
+            return std::nullopt;
+        }
+
+        std::ifstream instream(absolutePath(), std::ios::ate | std::ios::binary);
+
+        if (!instream.is_open())
+        {
+            logWarning("Failed to open file at '", m_file.string(), "' to read bytes.");
+            return std::nullopt;
+        }
+
+        const auto size = static_cast<size_t>(instream.tellg());
+        std::vector<std::byte> bytes(size);
+
+        instream.seekg(0);
+        
+        if (!instream.read(reinterpret_cast<char*>(bytes.data()), size))
+        {
+            return std::nullopt;
+        }
+
+        return bytes;
+    }
+}
