d96373000c2ab73621e6abeef09f39f95b5a574b
Steven Sell <ssell@vertexfragment.com>
2026-07-31 11:38:59 -0400

    Begin formalization of Task

--- tests/src/litl-core/task_tests.cpp
+++ tests/src/litl-core/task_tests.cpp
@@ -1,11 +1,14 @@
-#include "tests.hpp"
-#include "litl-core/thread.hpp"
-
 #include <coroutine>
 #include <filesystem>
 #include <optional>
 #include <semaphore>
 #include <thread>
+
+
+#include "tests.hpp"
+#include "litl-core/task/task.hpp"
+#include "litl-core/task/syncWait.hpp"
+#include "litl-core/thread.hpp"
 
 /**
  * I'll be honest.
@@ -129,123 +132,6 @@ namespace litl::tests
         // Task
         // ---------------------------------------------------------------------------------
 
-        template <typename T>
-        struct Task
-        {
-            /// <summary>
-            /// Again, the required internal structure that makes this a valid C++ coroutine.
-            /// </summary>
-            struct promise_type
-            {
-                struct final_awaiter
-                {
-                    bool await_ready() noexcept
-                    {
-                        return false;
-                    }
-
-                    std::coroutine_handle<> await_suspend(std::coroutine_handle<promise_type> handle) noexcept
-                    {
-                        return handle.promise().continuation;
-                    }
-
-                    void await_resume() noexcept
-                    {
-
-                    }
-                };
-
-                std::optional<T> result;
-                std::coroutine_handle<> continuation = std::noop_coroutine();
-
-                /// <summary>
-                /// Called first. Returns the outer coroutine object (Task or Generator).
-                /// </summary>
-                Task get_return_object()
-                {
-                    return Task{
-                        std::coroutine_handle<promise_type>::from_promise(*this)
-                    };
-                }
-
-                /// <summary>
-                /// Called before the coroutine body runs and returns lazy/eager on if it should be run immediately. We choose lazy.
-                /// </summary>
-                std::suspend_always initial_suspend() noexcept
-                {
-                    return {};
-                }
-
-                /// <summary>
-                /// Called after the coroutine finishes and must return an awaitable. We return our custom final_awaiter.
-                /// </summary>
-                /// <returns></returns>
-                final_awaiter final_suspend() noexcept
-                {
-                    return {};
-                }
-
-                /// <summary>
-                /// Required if the coroutine returns a value via co_return.
-                /// </summary>
-                void return_value(T v)
-                {
-                    result = std::move(v);
-                }
-
-                /// <summary>
-                /// Called if an exception escapes the coroutine body.
-                /// We compile with exceptions disabled, so that is not a concern for us.
-                /// </summary>
-                void unhandled_exception()
-                {
-
-                }
-            };
-
-            using coroutine_handle = std::coroutine_handle<promise_type>;
-            coroutine_handle handle;
-
-            explicit Task(coroutine_handle handle) : handle(handle) {}
-            ~Task() { if (handle) { handle.destroy(); } }
-            Task(Task&& other) noexcept : handle(std::exchange(other.handle, {})) {}
-            Task(Task const&) = delete;
-
-            /// <summary>
-            /// Required for an awaitable coroutine. Must return a bool.
-            /// If true, the coroutine does not suspend and immediately calls await_resume.
-            /// If false, the coroutine suspends and calls await_suspend.
-            /// </summary>
-            bool await_ready() const noexcept
-            {
-                return !handle || handle.done();
-            }
-
-            /// <summary>
-            /// Called when the coroutine resumts (when await_ready returns true).
-            /// The return type of this method dictates the result value of the co_await.
-            /// </summary>
-            /// <returns></returns>
-            T await_resume()
-            {
-                return std::move(*handle.promise().result);
-            }
-
-            /// <summary>
-            /// Called if await_ready returns false.
-            /// This comes in three flavors based on return type:
-            /// 
-            ///     void: Suspends and returns control to the caller.
-            ///     bool: If true suspends, if false returns execution immediately.
-            ///     std::coroutine_handle: Suspends the current coroutine and immediately executes the returned coroutine handle (symmetric transfer).
-            /// </summary>
-            std::coroutine_handle<> await_suspend(std::coroutine_handle<> awaiting) noexcept
-            {
-                handle.promise().continuation = awaiting;
-                return handle;
-            }
-        };
-
         /// <summary>
         /// Not really a thread pool, but good enough to demonstrate the coroutine being run on a separate thread.
         /// </summary>
@@ -345,74 +231,18 @@ namespace litl::tests
         /// <summary>
         /// Retrieves the file size from a worker thread and then hops back to the main/calling thread and returns the value.
         /// </summary>
-        /// <param name="path"></param>
-        /// <returns></returns>
         Task<size_t> loadFileSize(std::string path)
         {
+            /*
             co_await ResumeOnThreadPool{};
             workerThreadInfo = ThreadInfo::get();
             size_t size = static_cast<size_t>(std::filesystem::file_size(path));
             co_await ResumeOnMain{ GetMainThreadQueue() };
             co_return size;
-        }
+            */
 
-        namespace detail {
-            /// <summary>
-            /// A helper coroutine for implementing the synchronous wait used by the test.
-            /// It kinda defeats the purpose of an async coroutine as it blocks on the calling thread but it is useful for testing.
-            /// ... todo ... I still need to fully go through this ...
-            /// </summary>
-            struct SyncWaitCoro {
-                struct promise_type {
-                    std::binary_semaphore ready{ 0 };
-
-                    SyncWaitCoro get_return_object() noexcept {
-                        return SyncWaitCoro{ std::coroutine_handle<promise_type>::from_promise(*this) };
-                    }
-                    std::suspend_always initial_suspend() noexcept { return {}; }
-                    void return_void() noexcept {}
-                    void unhandled_exception() noexcept { std::terminate(); }  // body catches everything
-
-                    struct final_awaiter {
-                        static constexpr bool await_ready() noexcept { return false; }
-                        void await_suspend(std::coroutine_handle<promise_type> h) noexcept {
-                            h.promise().ready.release();      // coroutine is fully suspended now
-                        }
-                        static constexpr void await_resume() noexcept {}
-                    };
-                    final_awaiter final_suspend() noexcept { return {}; }
-                };
-                using handle = std::coroutine_handle<promise_type>;
-                handle h;
-                explicit SyncWaitCoro(handle h) noexcept : h(h) {}
-                ~SyncWaitCoro() { if (h) h.destroy(); }
-                SyncWaitCoro(const SyncWaitCoro&) = delete;   // lives on sync_wait's stack, never moves
-            };
-
-            template <class T>
-            SyncWaitCoro run(Task<T>& t, std::optional<T>& value, std::exception_ptr& err) {
-                value = co_await t;
-            }
-
-        } // namespace detail
-
-        /// <summary>
-        /// Forces a wait for the async coroutine to finish.
-        /// Defeats the purpose of the async coroutine but is needed for testing.
-        /// ... todo ... I still need to fully go through this ...
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="t"></param>
-        /// <returns></returns>
-        template <class T>
-        T sync_wait(Task<T> t) {
-            std::optional<T> value;
-            std::exception_ptr err;
-
-            auto coro = detail::run(t, value, err);
-            coro.h.resume();                    // starts run(), which co_awaits t, which starts t
-            coro.h.promise().ready.acquire();   // block until final_suspend
-            return std::move(*value);
+            size_t size = static_cast<size_t>(std::filesystem::file_size(path));
+            co_return size;
         }
     }
 
@@ -439,12 +269,18 @@ namespace litl::tests
 
     LITL_TEST_CASE("Task", "[core::tasks]")
     {
-        auto originalThread = ThreadInfo::get();
-        size_t size = sync_wait(loadFileSize("litl-tests.exe"));
-        auto resolvingThread = ThreadInfo::get();
-        auto workerThread = workerThreadInfo;
+        const auto originalThread = ThreadInfo::get();
+        
+        {
+            Task<size_t> loadFileSizeTask = loadFileSize("litl-tests.exe");
+            TaskStatus<size_t> taskResult = syncWait(loadFileSizeTask);
+            REQUIRE(taskResult.result.has_value());
+            REQUIRE(taskResult.result.value() > 0ull);
+        }
 
-        REQUIRE(size > 0ull);
+        const auto resolvingThread = ThreadInfo::get();           // make sure we are returning back to our original thread
+        const auto workerThread = workerThreadInfo;               // make sure the work itself was done on a separate thread
+
         REQUIRE(originalThread.index == resolvingThread.index);
         REQUIRE(originalThread.index != workerThread.index);
     } LITL_END_TEST_CASE
