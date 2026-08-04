115a7528439080656b927eeed0c3b91828a27d0c
Steven Sell <ssell@vertexfragment.com>
2026-08-04 00:02:41 -0400

    Task is running now, but needs to be cleaned up

--- litl/engine/include/litl-engine/tasks/taskManager.hpp
+++ litl/engine/include/litl-engine/tasks/taskManager.hpp
@@ -2,21 +2,32 @@
 #define LITL_ENGINE_TASKS_TASK_MANAGER_H__
 
 #include <coroutine>
+#include <memory>
+#include <mutex>
+#include <utility>
 
 #include "litl-core/authority.hpp"
 #include "litl-core/impl.hpp"
 #include "litl-core/task/task.hpp"
+#include "litl-core/task/taskThreadQueue.hpp"
 
 namespace litl
 {
     class Engine;
     class ServiceProvider;
+    class TaskThreadPool;
 
     /// <summary>
     /// Manages the engine-specific task thread pool.
     /// </summary>
     class TaskManager final
     {
+        struct OwnedTask final
+        {
+            ~OwnedTask() { if (handle) { handle.destroy(); } }
+            std::coroutine_handle<> handle;
+        };
+
     public:
 
         TaskManager();
@@ -30,17 +41,18 @@ namespace litl
         void update() noexcept;
 
         template<typename T>
-        void schedule(Task<T> task) noexcept
+        void schedule(Task<T>&& task) noexcept
         {
-            schedule(task.handle);
+            std::scoped_lock lock(m_mutex);
+            m_ownedTasks.emplace_back(std::exchange(task.handle, {}));
+            TaskThreadQueue::GetMainThreadQueue().schedule(m_ownedTasks.back().handle);
         }
 
     private:
 
-        void schedule(std::coroutine_handle<> handle) noexcept;
-
-        struct Impl;
-        ImplPtr<Impl, 256u> m_impl;
+        std::vector<OwnedTask> m_ownedTasks;
+        std::unique_ptr<TaskThreadPool> m_pTaskThreadPool;
+        std::mutex m_mutex;
     };
 }
 
