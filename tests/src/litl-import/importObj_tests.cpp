0853abd6edffd393d8cfe1ccdc944bb32f85791f
Steven Sell <ssell@vertexfragment.com>
2026-08-10 15:59:10 -0400

    Update LFS and skeleton obj test

---
+++ tests/src/litl-import/importObj_tests.cpp
@@ -0,0 +1,13 @@
+#include "tests.hpp"
+#include "litl-import/importService.hpp"
+
+namespace litl::tests
+{
+    LITL_TEST_CASE("Import OBJ", "[ecs::import::obj]")
+    {
+        import::ImportService importer{};
+
+
+
+    } LITL_END_TEST_CASE
+}
