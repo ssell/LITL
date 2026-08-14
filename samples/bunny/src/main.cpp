5b7f8377bfbd42cf9fd95b247ab530fd160c5c9b
Steven Sell <ssell@vertexfragment.com>
2026-08-14 00:37:39 -0400

    Sample placeholder code

--- samples/bunny/src/main.cpp
+++ samples/bunny/src/main.cpp
@@ -3,6 +3,7 @@
 using namespace litl;
 
 void bootstrap(ServiceProvider& services, EntityCommands& commands);
+MaterialHandle createPlaceholderMaterial(ObjectPool& objectPool);
 
 int main()
 {
@@ -37,4 +38,39 @@ void bootstrap(ServiceProvider& services, EntityCommands& commands)
     // ... in progress ...
 
     auto bunnyMesh = assets->getMesh("mesh/bunny");     // maps to "assets/mesh/bunny.litlmesh"
+    auto bunnyMaterial = createPlaceholderMaterial(*objectPool);
+
+    // ... todo below is dependent on finishing the mesh asset pipeline (gpu buffer upload) ...
+    /*
+    auto bunnyEntity = commands.createEntity();
+
+    Transform transform{};
+    transform.setPosition(vec3{ 0.0f, 0.0f, 5.0f });
+
+    commands.addComponent<Transform>(bunnyEntity, transform);
+    commands.addComponent<LocalBounds>(bunnyEntity, LocalBounds{});     // todo these need to come from the mesh ...
+    commands.addComponent<WorldBounds>(bunnyEntity, WorldBounds{});
+    commands.addComponent<MaterialRef>(bunnyEntity, MaterialRef{ .handle = bunnyMaterial });
+    commands.addComponent<MeshRef>(bunnyEntity, MeshRef{ .handle = bunnyMesh->handle });
+    */
+}
+
+MaterialHandle createPlaceholderMaterial(ObjectPool& objectPool)
+{
+    // ... todo this will eventually be defined by a material asset ...
+    auto spirvBytes = File("assets/shaders/spirv/flat.spv").readAllBytes();
+
+    return objectPool.createMaterial(MaterialDescriptor{
+        .objectInfo = ObjectDescriptor {.name = "Flat" },
+        .vertexShader = ShaderResourceDescriptor {
+            .resource = "flat.spv",
+            .entryPoint = "vertexMain",
+            .bytes = spirvBytes.value()
+        },
+        .fragmentShader = ShaderResourceDescriptor {
+            .resource = "flat.spv",
+            .entryPoint = "fragmentMain",
+            .bytes = spirvBytes.value()
+        }
+        });
 }
