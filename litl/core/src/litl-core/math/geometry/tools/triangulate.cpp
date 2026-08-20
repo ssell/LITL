7baaa890bdbbf919938518ebb7bba9cac79d5eb4
Steven Sell <ssell@vertexfragment.com>
2026-08-20 15:06:51 -0400

    Quad triangulation

--- litl/core/src/litl-core/math/geometry/tools/triangulate.cpp
+++ litl/core/src/litl-core/math/geometry/tools/triangulate.cpp
@@ -12,7 +12,7 @@ namespace litl
 
              // ... todo 
 
-             report.resultTriangleFaceCount++;
+             report.resultTriangleFaceCount += (faceIndexCount - 2u);
         }
 
         void triangulateQuad(uint32_t faceIndexCount, uint32_t firstIndexIndex, std::span<Vertex const> vertices, std::span<uint32_t const> sourceIndices, std::vector<uint32_t>& triangulatedIndices, MeshTriangulationReport& report) noexcept
@@ -20,9 +20,51 @@ namespace litl
             report.sourceFaceCount++;
             report.sourceQuadFaceCount++;
 
-            // ... todo 
+            /**
+             * A quad can be split into triangles along two separate diagonals:
+             * 
+             *     1─────────2            1─────────2
+             *     │       ╱ │            │ ╲       │
+             *     │     ╱   │     or     │   ╲     │
+             *     │   ╱     │            │     ╲   │
+             *     │ ╱       │            │       ╲ │
+             *     0─────────3            0─────────3
+             *     split on v0-v2        split on v1-v3
+             * 
+             * We simply find the length of the two diagonals (|v0-v2| vs |v1-v3|)
+             * and then split on the shorter of the two to avoid long, narrow slivers.
+             */
 
-            report.resultTriangleFaceCount++;
+            const uint32_t index0 = sourceIndices[firstIndexIndex + 0];
+            const uint32_t index1 = sourceIndices[firstIndexIndex + 1];
+            const uint32_t index2 = sourceIndices[firstIndexIndex + 2];
+            const uint32_t index3 = sourceIndices[firstIndexIndex + 3];
+
+            const float v0v2 = distanceSq(vertices[index0].position, vertices[index2].position);
+            const float v1v3 = distanceSq(vertices[index1].position, vertices[index3].position);
+
+            if (v0v2 < v1v3)
+            {
+                triangulatedIndices.push_back(index0);
+                triangulatedIndices.push_back(index1);
+                triangulatedIndices.push_back(index2);
+
+                triangulatedIndices.push_back(index2);
+                triangulatedIndices.push_back(index3);
+                triangulatedIndices.push_back(index0);
+            }
+            else
+            {
+                triangulatedIndices.push_back(index1);
+                triangulatedIndices.push_back(index3);
+                triangulatedIndices.push_back(index0);
+
+                triangulatedIndices.push_back(index1);
+                triangulatedIndices.push_back(index2);
+                triangulatedIndices.push_back(index3);
+            }
+
+            report.resultTriangleFaceCount += 2u;
         }
 
         void triangulateTriangle(uint32_t faceIndexCount, uint32_t firstIndexIndex, std::span<Vertex const> vertices, std::span<uint32_t const> sourceIndices, std::vector<uint32_t>& triangulatedIndices, MeshTriangulationReport& report) noexcept
