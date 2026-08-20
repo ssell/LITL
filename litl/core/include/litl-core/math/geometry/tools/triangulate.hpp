2450bc344fc47a47c8c5ad88e85efd815a800e93
Steven Sell <ssell@vertexfragment.com>
2026-08-20 14:50:28 -0400

    Start on mesh triangulation

---
+++ litl/core/include/litl-core/math/geometry/tools/triangulate.hpp
@@ -0,0 +1,48 @@
+#ifndef LITL_MATH_GEOMETRY_TOOLS_MESH_TRIANGULATE_H__
+#define LITL_MATH_GEOMETRY_TOOLS_MESH_TRIANGULATE_H__
+
+#include <cstdint>
+#include <span>
+#include <vector>
+
+#include "litl-core/math/geometry/vertex.hpp"
+
+namespace litl
+{
+    struct MeshTriangulationReport
+    {
+        /// <summary>
+        /// The number of faces in the original source data.
+        /// </summary>
+        uint32_t sourceFaceCount = 0u;
+
+        /// <summary>
+        /// The number of triangles in the original source data.
+        /// </summary>
+        uint32_t sourceTriangleFaceCount = 0u;
+
+        /// <summary>
+        /// The number of quads in the original source data.
+        /// </summary>
+        uint32_t sourceQuadFaceCount = 0u;
+
+        /// <summary>
+        /// The number of ngons in the original source data.
+        /// </summary>
+        uint32_t sourceNgonFaceCount = 0u;
+
+        /// <summary>
+        /// The number of triangle faces after triangulation finished.
+        /// </summary>
+        uint32_t resultTriangleFaceCount = 0u;
+
+        /// <summary>
+        /// Was triangulation a success?
+        /// </summary>
+        bool success = false;
+    };
+
+    [[nodiscard]] MeshTriangulationReport triangulateMesh(std::span<Vertex const> vertices, std::span<uint32_t const> sourceIndices, std::vector<uint32_t>& triangulatedIndices, std::vector<uint32_t>& faceIndexCounts) noexcept;
+}
+
+#endif
