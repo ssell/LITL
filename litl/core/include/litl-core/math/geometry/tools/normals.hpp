de0b8f8b1234ace47290bec3a01e93e78e0fc709
Steven Sell <ssell@vertexfragment.com>
2026-08-20 15:50:09 -0400

    ngon face normal calculation using Newell's method

--- litl/core/include/litl-core/math/geometry/tools/normals.hpp
+++ litl/core/include/litl-core/math/geometry/tools/normals.hpp
@@ -25,7 +25,7 @@ namespace litl
     /// </summary>
     [[nodiscard]] constexpr vec3 faceNormalScaled(vec3 a, vec3 b, vec3 c) noexcept
     {
-        return cross(c - a, b - a);    // face area = (length / 2)
+        return cross(c - a, b - a);    // face area = (length / 2); area near 0 = degenerate triangle
     }
 
     /// <summary>
@@ -42,7 +42,109 @@ namespace litl
     /// </summary>
     [[nodiscard]] constexpr vec3 faceNormalScaledCCW(vec3 a, vec3 b, vec3 c) noexcept
     {
-        return cross(b - a, c - a);    // face area = (length / 2)
+        return cross(b - a, c - a);    // face area = (length / 2); area near 0 = degenerate triangle
+    }
+    
+    /// <summary>
+    /// Calculates the scaled normal for a set of vertex positions defining an n-gon in counter-clockwise order.
+    /// The scaled, unnormalized, value is typically used for area weighting, degeneracy tests, etc.
+    /// </summary>
+    [[nodiscard]] constexpr vec3 ngonFaceNormalScaledCCW(std::span<vec3 const> face) noexcept
+    {
+        // Using Newell's method
+        vec3 normal{};
+
+        if (face.size() >= 3)
+        {
+            for (size_t i = 0, j = face.size() - 1; i < face.size(); j = i++)       // note that j starts at the end, and then follows one behind
+            {
+                const vec3& curr = face[j];
+                const vec3& next = face[i];
+
+                normal.x() += (curr.y() - next.y()) * (curr.z() + next.z());        // note this looks like the cross product, but its not.
+                normal.y() += (curr.z() - next.z()) * (curr.x() + next.x());
+                normal.z() += (curr.x() - next.x()) * (curr.y() + next.y());
+            }
+        }
+
+        return normal;
+    }
+
+    /// <summary>
+    /// Calculates the normal for a set of vertex positions defining an n-gon in counter-clockwise order.
+    /// </summary>
+    [[nodiscard]] constexpr vec3 ngonFaceNormalCCW(std::span<vec3 const> face) noexcept
+    {
+        return ngonFaceNormalScaledCCW(face).normalized();
+    }
+
+    /// <summary>
+    /// Calculates the scaled normal for a set of vertex positions defining an n-gon in clockwise order.
+    /// The scaled, unnormalized, value is typically used for area weighting, degeneracy tests, etc.
+    /// </summary>
+    [[nodiscard]] constexpr vec3 ngonFaceNormalScaled(std::span<vec3 const> face) noexcept
+    {
+        // Negate the accumulated normal to flip the winding to clockwise
+        return -ngonFaceNormalScaledCCW(face);
+    }
+
+    /// <summary>
+    /// Calculates the normal for a set of vertex positions defining an n-gon in counter-clockwise order.
+    /// </summary>
+    [[nodiscard]] constexpr vec3 ngonFaceNormal(std::span<vec3 const> face) noexcept
+    {
+        return ngonFaceNormalScaled(face).normalized();
+    }
+
+    /// <summary>
+    /// Calculates the scaled normal for a set of vertices defining an n-gon in counter-clockwise order.
+    /// The scaled, unnormalized, value is typically used for area weighting, degeneracy tests, etc.
+    /// </summary>
+    [[nodiscard]] constexpr vec3 ngonFaceNormalScaledCCW(std::span<Vertex const> face) noexcept
+    {
+        // Using Newell's method
+        vec3 normal{};
+
+        if (face.size() >= 3)
+        {
+            for (size_t i = 0, j = face.size() - 1; i < face.size(); j = i++)       // note that j starts at the end, and then follows one behind
+            {
+                const vec3& curr = face[j].position;
+                const vec3& next = face[i].position;
+
+                normal.x() += (curr.y() - next.y()) * (curr.z() + next.z());        // note this looks like the cross product, but its not.
+                normal.y() += (curr.z() - next.z()) * (curr.x() + next.x());
+                normal.z() += (curr.x() - next.x()) * (curr.y() + next.y());
+            }
+        }
+
+        return normal;
+    }
+
+    /// <summary>
+    /// Calculates the normal for a set of vertices defining an n-gon in counter-clockwise order.
+    /// </summary>
+    [[nodiscard]] constexpr vec3 ngonFaceNormalCCW(std::span<Vertex const> face) noexcept
+    {
+        return ngonFaceNormalScaledCCW(face).normalized();
+    }
+
+    /// <summary>
+    /// Calculates the scaled normal for a set of vertices defining an n-gon in clockwise order.
+    /// The scaled, unnormalized, value is typically used for area weighting, degeneracy tests, etc.
+    /// </summary>
+    [[nodiscard]] constexpr vec3 ngonFaceNormalScaled(std::span<Vertex const> face) noexcept
+    {
+        // Negate the accumulated normal to flip the winding to clockwise
+        return -ngonFaceNormalScaledCCW(face);
+    }
+
+    /// <summary>
+    /// Calculates the normal for a set of vertices defining an n-gon in counter-clockwise order.
+    /// </summary>
+    [[nodiscard]] constexpr vec3 ngonFaceNormal(std::span<Vertex const> face) noexcept
+    {
+        return ngonFaceNormalScaled(face).normalized();
     }
 
     /// <summary>
