bfd888712a60ccf21fb880aa87c67195266ef155
Steven Sell <ssell@vertexfragment.com>
2026-08-20 22:09:36 -0400

    Reimplement project2d using orthonormalBasis + test

--- tests/src/litl-core/math/uncommon_tests.cpp
+++ tests/src/litl-core/math/uncommon_tests.cpp
@@ -3,27 +3,33 @@
 
 namespace litl::tests
 {
-    LITL_TEST_CASE("orthonormal basis right-handed", "[math::uncommon]")
+    LITL_TEST_CASE("orthonormalBasis", "[math::uncommon]")
     {
-        vec3 normal = vec3::up();
         vec3 forward, right;
 
-        orthonormalBasisRH(normal, right, forward);
+        orthonormalBasis(vec3::up(), right, forward);
 
-        REQUIRE(normal == vec3::up());
+        REQUIRE(right == vec3::right());
+        REQUIRE(forward == vec3::forward());
+    } LITL_END_TEST_CASE
+
+    LITL_TEST_CASE("orthonormalBasisRH", "[math::uncommon]")
+    {
+        vec3 forward, right;
+
+        orthonormalBasisRH(vec3::up(), right, forward);
+
         REQUIRE(right == vec3::right());
         REQUIRE(forward == -vec3::forward());
     } LITL_END_TEST_CASE
-        
-    LITL_TEST_CASE("orthonormal basis left-handed", "[math::uncommon]")
+
+    LITL_TEST_CASE("project2d", "[math::uncommon]")
     {
-        vec3 normal = vec3::up();
-        vec3 forward, right;
+        const vec3 normal = vec3::up();
+        const vec3 point = vec3{ 50.0f, 100.0f, 150.0f };
+        const vec3 origin = vec3::zero();
+        const vec2 point2d = project2d(normal, point, origin);
 
-        orthonormalBasis(normal, right, forward);
-
-        REQUIRE(normal == vec3::up());
-        REQUIRE(right == vec3::right());
-        REQUIRE(forward == vec3::forward());
+        REQUIRE(point2d == vec2{ 50.0f, 150.0f });
     } LITL_END_TEST_CASE
 }
