1fd3225aad81ee333a82a52749ec0be8fb117c26
Steven Sell <ssell@vertexfragment.com>
2026-08-25 23:26:28 -0400

    Update bunny sample to create 441 bunnies of varying colors

--- samples/bunny/src/main.cpp
+++ samples/bunny/src/main.cpp
@@ -3,7 +3,8 @@
 using namespace litl;
 
 void bootstrap(ServiceProvider& services, EntityCommands& commands);
-MaterialRef createPlaceholderMaterial(ObjectPool& objectPool);
+MaterialHandle createPlaceholderMaterial(ObjectPool& objectPool);
+void createBunny(EntityCommands& commands, MeshHandle meshHandle, MaterialHandle materialHandle, Material* material, vec3 position, color color);
 
 int main()
 {
@@ -27,51 +28,39 @@ void bootstrap(ServiceProvider& services, EntityCommands& commands)
     auto sceneView = services.get<SceneView>();         // A view into the current active scene.
     auto assets = services.get<AssetManager>();
 
-    const vec3 cameraPos = vec3{ 0.0f, 1.0f, 0.0f };
-    const vec3 bunnyPos = vec3{ 0.0f, 0.0f, 3.0f };
-
     createMainCamera(
         CameraDescriptor{ .projection = CameraProjection::Perspective, .clearColor = color{ 0.035f, 0.035f, 0.05f } }, 
-        cameraPos, 
-        bunnyPos,
+        vec3::zero(),
+        vec3::forward(),
         vec3::up(), 
         *objectPool, 
         *sceneView);
 
     // ... in progress ...
 
-    auto bunnyMesh = assets->getMesh("mesh/bunny");                     // maps to "assets/mesh/bunny.litlmesh"
-    auto bunnyMaterialRef = createPlaceholderMaterial(*objectPool);
-    auto bunnyEntity = commands.createEntity();
+    auto* bunnyMesh = assets->getMesh("mesh/bunny");                             // maps to "assets/mesh/bunny.litlmesh"
+    auto bunnyMaterialHandle = createPlaceholderMaterial(*objectPool);
+    auto* bunnyMaterial = objectPool->getMaterial(bunnyMaterialHandle);
 
-    commands.addComponent<Transform>(bunnyEntity, Transform::create(bunnyPos - vec3::right() * 0.75f));
-    commands.addComponent<LocalBounds>(bunnyEntity, LocalBounds{});     // todo these need to come from the mesh ...
-    commands.addComponent<WorldBounds>(bunnyEntity, WorldBounds{});
-    commands.addComponent<MaterialRef>(bunnyEntity, bunnyMaterialRef);
-    commands.addComponent<MeshRef>(bunnyEntity, MeshRef{ .handle = bunnyMesh->handle });
-
-    auto* material = objectPool->getMaterial(bunnyMaterialRef.handle);
-
-    material->setColor("tint"_sid, colors::Orange, bunnyMaterialRef.slot);
-    material->setFloat("fade"_sid, 1.0f, bunnyMaterialRef.slot);
-
-    // --- test second bunny
-
-    auto bunnyEntity2 = commands.createEntity();
-    auto bunnyEntity2MaterialRef = bunnyMaterialRef;
-    bunnyEntity2MaterialRef.slot = material->allocateSlot();
-
-    commands.addComponent<Transform>(bunnyEntity2, Transform::create(bunnyPos + vec3::right() * 0.75f));
-    commands.addComponent<LocalBounds>(bunnyEntity2, LocalBounds{});     // todo these need to come from the mesh ...
-    commands.addComponent<WorldBounds>(bunnyEntity2, WorldBounds{});
-    commands.addComponent<MaterialRef>(bunnyEntity2, bunnyEntity2MaterialRef);
-    commands.addComponent<MeshRef>(bunnyEntity2, MeshRef{ .handle = bunnyMesh->handle });
-
-    material->setColor("tint"_sid, colors::Green, bunnyEntity2MaterialRef.slot);
-    material->setFloat("fade"_sid, 1.0f, bunnyEntity2MaterialRef.slot);
+    if (bunnyMesh->handle.isValid() && bunnyMaterialHandle.isValid() && (bunnyMaterial != nullptr))
+    {
+        for (int32_t x = -10; x <= 10; ++x)
+        {
+            for (int32_t y = -10; y < 10; ++y)
+            {
+                createBunny(
+                    commands, 
+                    bunnyMesh->handle, 
+                    bunnyMaterialHandle, 
+                    bunnyMaterial, 
+                    vec3{static_cast<float>(x) * 1.5f, static_cast<float>(y) * 1.5f, 28.0f },
+                    color{ static_cast<float>(x + 10) * 0.05f, static_cast<float>(y + 10) * 0.05f, 0.0f });
+            }
+        }
+    }
 }
 
-MaterialRef createPlaceholderMaterial(ObjectPool& objectPool)
+MaterialHandle createPlaceholderMaterial(ObjectPool& objectPool)
 {
     // ... todo this will eventually be defined by a material asset ...
     auto spirvBytes = File("assets/shaders/spirv/test.spv").readAllBytes();
@@ -90,8 +79,24 @@ MaterialRef createPlaceholderMaterial(ObjectPool& objectPool)
         }
     });
 
-    return MaterialRef{
+    return materialHandle;
+}
+
+void createBunny(EntityCommands& commands, MeshHandle meshHandle, MaterialHandle materialHandle, Material* material, vec3 position, color color)
+{
+    auto entity = commands.createEntity();
+
+    const MaterialRef materialRef{
         .handle = materialHandle,
-        .slot = objectPool.getMaterial(materialHandle)->allocateSlot()
+        .slot = material->allocateSlot()
     };
+
+    commands.addComponent<Transform>(entity, Transform::create(position));
+    commands.addComponent<LocalBounds>(entity, LocalBounds{});     // todo these need to come from the mesh ...
+    commands.addComponent<WorldBounds>(entity, WorldBounds{});
+    commands.addComponent<MaterialRef>(entity, materialRef);
+    commands.addComponent<MeshRef>(entity, MeshRef{ .handle = meshHandle });
+
+    material->setColor("tint"_sid, color, materialRef.slot);
+    material->setFloat("fade"_sid, 1.0f, materialRef.slot);
 }
