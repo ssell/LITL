8ddf5cac37a624db825ad1120aa39063ff884d02
Steven Sell <ssell@vertexfragment.com>
2026-09-18 12:55:00 -0400

    TextureIntermediateData

--- litl/import/include/litl-import/texture/intermediate/textureIntermediateData.hpp
+++ litl/import/include/litl-import/texture/intermediate/textureIntermediateData.hpp
@@ -1,15 +1,70 @@
 #ifndef LITL_IMPORT_TEXTURE_INTERMEDIATE_DATA_H__
 #define LITL_IMPORT_TEXTURE_INTERMEDIATE_DATA_H__
 
+#include <cstdint>
+#include <span>
+#include <vector>
+
+#include "litl-renderer/enums.hpp"
+
 namespace litl::import
 {
+    /// <summary>
+    /// How the texture is to be interpreted.
+    /// </summary>
+    enum class TextureSemantic
+    {
+        Unknown       = 0u,
+        Albedo        = 1u,     // Color data (RGBA8_SRB)
+        NormalTangent = 2u,     // Tangent-space normal map (RGBA8_UNORM)
+        Mask          = 3u,     // Masking texture (RGBA8_UNORM)
+        Hdr           = 4u      // High-dynamic range texture (RGBA8_UNORM)
+    };
+
+    struct TextureLevel
+    {
+        uint64_t byteOffset{ 0ull };
+        uint64_t byteSize{ 0ull };
+        uint32_t width{ 0u };
+        uint32_t height{ 0u };
+        uint32_t depth{ 0u };
+    };
+
+    struct TextureDataDescriptor
+    {
+        DataFormat format{ DataFormat::Undefined };
+        uint32_t width{ 1u };
+        uint32_t height{ 1u };
+        uint32_t depth{ 1u };
+        uint32_t arrayLayers{ 1u };
+        TextureSemantic semantic{ TextureSemantic::Unknown };
+        bool isCubeMap{ false };
+        bool alpahPremultiplied{ false };
+    };
+
+    /// <summary>
+    /// Intermediate representation of a texture. All external texture formats converge onto this shape.
+    /// It should be noted that all formats are expanded to RGBA for both consistency and hardware support
+    /// as RGB formats (for example VK_FORMAT_R8G8B8_*) are frequently unsupported as sampled images.
+    /// </summary>
     class TextureIntermediateData
     {
     public:
 
-        // ... todo ...
+        TextureDataDescriptor& getDataDescriptorWriteRef() noexcept;
+        TextureDataDescriptor const& getDataDescriptor() const noexcept;
+
+        std::vector<TextureLevel>& getTextureLevelsWriteRef() noexcept;
+        std::span<TextureLevel const> getTextureLevels() const noexcept;
+
+        std::vector<std::byte>& getPixelBytesWriteRef() noexcept;
+        std::span<std::byte const> getPixelBytes() const noexcept;
 
     private:
+
+        TextureDataDescriptor m_dataDescriptor{};
+        std::vector<TextureLevel> m_levels;             // m_levels[0] is always present. Mipmap generation appends additional levels.
+        std::vector<std::byte> m_pixels;                // raw pixel data. m_levels indexes into this.
     };
 }
 
--- litl/import/src/texture/intermediate/textureIntermediateData.cpp
+++ litl/import/src/texture/intermediate/textureIntermediateData.cpp
@@ -2,5 +2,33 @@
 
 namespace litl::import
 {
-    // ... todo ...
+    TextureDataDescriptor& TextureIntermediateData::getDataDescriptorWriteRef() noexcept
+    {
+        return m_dataDescriptor;
+    }
+
+    TextureDataDescriptor const& TextureIntermediateData::getDataDescriptor() const noexcept
+    {
+        return m_dataDescriptor;
+    }
+
+    std::vector<TextureLevel>& TextureIntermediateData::getTextureLevelsWriteRef() noexcept
+    {
+        return m_levels;
+    }
+
+    std::span<TextureLevel const> TextureIntermediateData::getTextureLevels() const noexcept
+    {
+        return m_levels;
+    }
+
+    std::vector<std::byte>& TextureIntermediateData::getPixelBytesWriteRef() noexcept
+    {
+        return m_pixels;
+    }
+
+    std::span<std::byte const> TextureIntermediateData::getPixelBytes() const noexcept
+    {
+        return m_pixels;
+    }
 }
