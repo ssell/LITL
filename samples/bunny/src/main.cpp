6f909bcea747caa6ce0311813a5d1d713c647631
Steven Sell <ssell@vertexfragment.com>
2026-09-23 14:29:17 -0400

    Test cube in prep for texture testing

--- samples/bunny/src/main.cpp
+++ samples/bunny/src/main.cpp
@@ -13,7 +13,8 @@ namespace litl::samples
         auto objectPool = services.get<ObjectPool>();
         auto sceneView = services.get<SceneView>();
         auto assets = services.get<AssetManager>();
-        
+
+        /*
         createMainCamera(color{ 0.015f, 0.015f, 0.025f }, vec3(-5.0f, 1.5f, 0.0f), vec3(5.0f, 1.5f, 0.0f), vec3::up(), *objectPool, *sceneView);
 
         const auto bunnyEntity = commands.createEntity();
@@ -27,6 +28,15 @@ namespace litl::samples
         commands.addComponent<PendingModelInstance>(sponzaEntity, createModelInstance("models/sponza", "materials/flat", *assets));
         commands.addComponent<LocalBounds>(sponzaEntity);
         commands.addComponent<Spin>(bunnyEntity);
+        */
+
+        createMainCamera(color{ 0.015f, 0.015f, 0.025f }, vec3{ 0.0f, 2.0f, 0.0f }, vec3{ 0.0f, 0.0f, 5.0f }, vec3::up(), *objectPool, *sceneView);
+
+        const auto cubeEntity = commands.createEntity();
+        commands.addComponent<Transform>(cubeEntity, Transform::create(vec3{ 0.0f, 0.0f, 5.0f }));
+        commands.addComponent<PendingModelInstance>(cubeEntity, createModelInstance("models/cube", "materials/flat", *assets));
+        commands.addComponent<LocalBounds>(cubeEntity);
+        commands.addComponent<Spin>(cubeEntity);
     }
 }
 
